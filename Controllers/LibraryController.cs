0c72050017d250312d8cdd341cbb7728bf316da8
Chathuminipamodya <138112305+Chathuminipamodya@users.noreply.github.com>
2026-01-12 23:58:58 +0530

    JWT token

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -8,12 +8,14 @@ namespace Library.Controllers
     [ApiController]
     public class LibraryController : ControllerBase
     {
-        public LibraryController(Context context, EmailService emailService) { 
+        public LibraryController(Context context, EmailService emailService, JwtService jwtService) { 
             Context = context;
             EmailService = emailService;
+            JwtService = jwtService;
         }
         public Context Context { get; }
         public EmailService EmailService { get; }
+        public JwtService JwtService { get; }
 
         [HttpPost("Register")]
         public ActionResult Register(User user)
@@ -45,5 +47,27 @@ namespace Library.Controllers
                         Once it is aprooved, you will get an email.");
             
         }
+        [HttpGet("Login")]
+        public ActionResult Login(string email, string password)
+        {
+            if (Context.Users.Any(u => u.Email.Equals(email) && u.Password.Equals(password)))
+            {
+                var user = Context.Users.Single(user => user.Email.Equals(email) && user.Password.Equals(password));
+
+                if (user.AccountStatus == AccountStatus.UNAPROOVED)
+                {
+                    return Ok("unapproved");
+                }
+
+                if (user.AccountStatus == AccountStatus.BLOCKED)
+                {
+                    return Ok("blocked");
+                }
+
+                return Ok(JwtService.GenerateToken(user));
+            }
+            return Ok("not found");
+        }
+
     }
 }
